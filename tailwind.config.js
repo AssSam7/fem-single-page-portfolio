fe90e50a3cb9e7925fecbeda09a2dbac7e9d5650
Aslam Mohammed <mohammadaslamparvez7@gmail.com>
2024-09-05 00:12:59 +0530

    Customising the themes and typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,32 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    colors: {
+      black: "#030303",
+      "medium-brown": "#7A746E",
+      "light-cream": "#FFF7F0",
+      "galactic-blue": "#755CDE",
+      "summer-yellow": "#F6A560",
+      pink: "#F39E9E",
+      "light-red": "#EB7565",
+      cyan: "#61C4B7",
+      "dark-purple": "#552049",
+    },
+    fontFamily: {
+      sans: ["Plus Jakarta Sans", "sans-serif"],
+    },
+    fontSize: {
+      sm: ["18px", "28px"],
+      base: ["32px", "40px"],
+      lg: ["40px", "51px"],
+      xl: ["56px", "70px"],
+    },
     extend: {},
   },
   plugins: [],
