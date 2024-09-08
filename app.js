84175983d61fdbd7c0d06b62d1b194ec09104f25
Aslam Mohammed <mohammadaslamparvez7@gmail.com>
2024-09-08 19:58:13 +0530

    Dynamic Javascript based on Viewport Width

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
+/* DOM Elements */
 const worksSlider = document.querySelector("#works-slider");
 const arrowLeft = document.querySelector("#arrow-left");
 const arrowRight = document.querySelector("#arrow-right");
 
-arrowRight.addEventListener("click", () => {
+let width = document.documentElement.clientWidth || window.innerWidth;
+
+function arrowRightDesktopHandler() {
   const refId = parseInt(worksSlider.firstElementChild.dataset["id"]) + 3;
   const newId = refId > 5 ? refId - 5 : refId;
   const nextImgDivHTML = `<div data-id="${newId}">
@@ -16,9 +19,9 @@ arrowRight.addEventListener("click", () => {
   worksSlider.removeChild(worksSlider.firstElementChild);
 
   worksSlider.insertAdjacentHTML("beforeend", nextImgDivHTML);
-});
+}
 
-arrowLeft.addEventListener("click", () => {
+function arrowLeftDesktopHandler() {
   const refId = parseInt(worksSlider.lastElementChild.dataset["id"]) - 3;
   console.log(refId);
   const newId = refId < 1 ? 5 - Math.abs(refId) : refId;
@@ -33,4 +36,21 @@ arrowLeft.addEventListener("click", () => {
   worksSlider.removeChild(worksSlider.lastElementChild);
 
   worksSlider.insertAdjacentHTML("afterbegin", nextImgDivHTML);
+}
+
+if (width > 768) {
+  arrowRight.addEventListener("click", arrowRightDesktopHandler);
+  arrowLeft.addEventListener("click", arrowLeftDesktopHandler);
+}
+
+window.addEventListener("resize", (e) => {
+  width = e.target.outerWidth;
+
+  if (width > 768) {
+    arrowRight.addEventListener("click", arrowRightDesktopHandler);
+    arrowLeft.addEventListener("click", arrowLeftDesktopHandler);
+  } else {
+    arrowRight.removeEventListener("click", arrowRightDesktopHandler);
+    arrowLeft.removeEventListener("click", arrowLeftDesktopHandler);
+  }
 });
